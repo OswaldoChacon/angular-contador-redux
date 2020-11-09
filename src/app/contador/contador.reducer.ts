fa918cab5e9b78f30d7d0008de1e36ce6b0dbb00
Oswaldo de Jesus Chacon Perez <39079647+OswaldoChacon@users.noreply.github.com>
2020-11-09 12:15:25 -0600

    implementando reducer de ngrx
    cambiando el reducer propio usando switch con el reducer de ngrx

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,23 +1,27 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { incrementar, decrementar, reset } from './contador.actions';
 
-export const initialState = 0;
+export const initialState = 20;
 
-const _counterReducer = createReducer(
+const _contadorReducer = createReducer(
     initialState,
     on(incrementar, (state) => state + 1),
     on(decrementar, (state) => state - 1),
     on(reset, (state) => 0)
 );
 
-export function contadorReducer(state = 10, action: Action) {
-    switch (action.type) {
-        case incrementar.type:
-            return state + 1;
-        case decrementar.type:
-            return state - 1;
-        default:
-            return state;
-    }
-    return _counterReducer(state, action);
+export function contadorReducer(state, action) {
+    return _contadorReducer(state, action);
 }
+
+
+// export function contadorReducer(state = 10, action: Action) {
+//     switch (action.type) {
+//         case incrementar.type:
+//             return state + 1;
+//         case decrementar.type:
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
