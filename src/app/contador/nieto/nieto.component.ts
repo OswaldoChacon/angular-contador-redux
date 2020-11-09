ae0ec0ab357df993f37d0497b73af2af9f9d5dab
Oswaldo de Jesus Chacon Perez <39079647+OswaldoChacon@users.noreply.github.com>
2020-11-09 17:42:56 -0600

    agregar componente nieto
    agregar componente nieto para realizar el reset del contador en todos los otros componentes

--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,4 +1,7 @@
+import { reset } from './../contador.actions';
+import { Store } from '@ngrx/store';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AppState } from 'src/app/app.reducers';
 
 @Component({
   selector: 'app-nieto',
@@ -8,15 +11,20 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class NietoComponent implements OnInit {
 
-  @Input() contador: number;
-  @Output() cambioContador = new EventEmitter<number>();
-  constructor() { }
+  // @Input() contador: number;
+  // @Output() cambioContador = new EventEmitter<number>();
+  contador: number;
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.store.select('contador').subscribe(contador => this.contador = contador);
   }
 
   reset() {
-    this.contador = 0;
-    this.cambioContador.emit(0);
+    this.store.dispatch(reset());
+    // this.contador = 0;
+    // this.cambioContador.emit(0);
   }
 }
