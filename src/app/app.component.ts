2fe56377c3480118a3048c0299d05d9a1518e82a
Oswaldo de Jesus Chacon Perez <39079647+OswaldoChacon@users.noreply.github.com>
2020-11-09 18:04:45 -0600

    uso del pipe async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { decrementar, incrementar } from './contador/contador.actions';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './app.reducers'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,17 @@ import { AppState } from './app.reducers'
 export class AppComponent implements OnInit {
   title = 'contador-redux';
 
-  contador: number;
+  // contador: number;
+  contador$: Observable<number>
   constructor(
     private store: Store<AppState>
   ) {
-    this.contador = 0
+    // this.contador = 0
+    this.contador$ = store.select('contador');
   }
 
   ngOnInit() {
-    this.store.select('contador').subscribe(contador => this.contador = contador)
+    // this.store.select('contador').subscribe(contador => this.contador = contador)
   }
 
   incrementar() {
